d3acef0d1541c6b067dcad049e7e8f990cb74133
Chris Anders <github@hell-labs.de>
2012-06-30 23:53:35 +0200

    Added code to hide/show all entries when clicking on the header.

--- week3/hideShow.js
+++ week3/hideShow.js
@@ -13,4 +13,17 @@ $(document).ready(function () {
 			$(this).next().toggle('slow');
 		}
 	});
+	
+	$("h1").click(function() {
+		if(!$("h2").next().hasClass("change")) {
+			$("h2").next().toggle('slow');
+			$("h2").next().addClass("change");
+			$("h2").parent().css("background", '#a9a9a9');
+		}
+		else {
+			$("h2").parent().css("background", '#d3d3d3');
+			$("h2").next().removeClass();
+			$("h2").next().toggle('slow');
+		}
+	});
 });
